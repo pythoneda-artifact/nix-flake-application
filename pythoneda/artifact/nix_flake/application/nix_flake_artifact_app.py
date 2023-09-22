e3078d5f515436ce48538ff46c0b3c26349b011b
rydnr <github@acm-sl.org>
2023-09-22 18:06:48 +0200

    Minor fix

--- pythoneda/artifact/nix_flake/application/nix_flake_artifact_app.py
+++ pythoneda/artifact/nix_flake/application/nix_flake_artifact_app.py
@@ -20,6 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import asyncio
 from pythoneda.application import PythonEDA
+from pythoneda.artifact.nix_flake.infrastructure import NixFlakeGitRepo
 
 class NixFlakeArtifactApp(PythonEDA):
     """
