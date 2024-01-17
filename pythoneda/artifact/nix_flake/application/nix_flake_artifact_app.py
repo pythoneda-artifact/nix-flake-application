f38ef7324d8f94a20b0d614cf6e40f145b7d47cb
rydnr <github@acm-sl.org>
2024-01-17 19:12:22 +0100

    Added vim and emacs settings

--- pythoneda/artifact/nix_flake/application/nix_flake_artifact_app.py
+++ pythoneda/artifact/nix_flake/application/nix_flake_artifact_app.py
@@ -67,3 +67,11 @@ if __name__ == "__main__":
             "pythoneda.artifact.nix_flake.application.NixFlakeArtifactApp"
         )
     )
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
